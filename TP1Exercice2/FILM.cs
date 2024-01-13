fd81150dce312d7f8c392e1054ae400591529268
yarou007 <yacine.hami@esen.tn>
2024-01-13 18:36:50 +0100

    The time i was out, they pull be back in

---
+++ TP1Exercice2/FILM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TP1Exercice2
+{
+    internal class FILM
+    {
+        string titre;
+        string nomActeur;
+        int anneeProd;
+
+        public FILM(string titre, string nomActeur, int anneeProd)
+        {
+            this.Titre = titre;
+            this.NomActeur = nomActeur;
+            this.AnneeProd = anneeProd;
+        }
+
+        public string Titre { get => titre; set => titre = value; }
+        public string NomActeur { get => nomActeur; set => nomActeur = value; }
+        public int AnneeProd { get => anneeProd; set => anneeProd = value; }
+
+        public override string ToString()
+        {
+            return "Film: " + this.titre + " " + this.anneeProd + " "+ this.nomActeur;
+        }
+
+
+    }
+}
---
+++ TP1Exercice2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TP1Exercice2;
+
+namespace TP1Exerice1
+{
+    public static class Porgram
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("Donner nombre de film a ajouter : ");
+            int n = int.Parse(Console.ReadLine());
+
+            FILM[] tabFilm= new FILM[n];
+       
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Donner titre: ");
+                string nf = Console.ReadLine();
+                Console.WriteLine("Donner Nom acteur: ");
+                string na = Console.ReadLine();
+                Console.WriteLine("Donner annee: ");
+                int an = int.Parse(Console.ReadLine());
+                FILM e = new FILM(nf, na,an);
+                tabFilm[i] = e;
+            }
+            Console.WriteLine("Affichage tableau de film : ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(tabFilm[i].ToString());
+            }
+            
+            
+
+
+        }
+    }
+}
